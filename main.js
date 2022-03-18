2eb314156ff89d24cd6b149621fec4eb8f5624f8
KrishaSuda <85116052+KrishaSuda@users.noreply.github.com>
2022-03-18 14:53:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/kRWZHkblD/model.json", modelReady)
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results){
+    if (error){
+        console.error(error);
+    } else {};
